5e2543811d4d77343d6ddb5ec453cc6373102b10
vyPal <kubik.palacky@gmail.com>
2025-01-24 08:20:07 +0100

    Remove join event

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,11 @@
-use async_trait::async_trait;
-use pumpkin_api_macros::{plugin_impl, plugin_method, with_runtime};
-use pumpkin::plugin::{player::{join::PlayerJoinEventImpl, PlayerEvent, PlayerJoinEvent}, Context, EventHandler, EventPriority};
-use pumpkin_util::text::{color::NamedColor, TextComponent};
-
-struct MyJoinHandler;
-
-#[with_runtime(global)]
-#[async_trait]
-impl EventHandler<PlayerJoinEventImpl> for MyJoinHandler {
-    async fn handle_blocking(&self, event: &mut PlayerJoinEventImpl) {
-        event.set_join_message(
-            TextComponent::text(format!("Welcome, {}!", event.get_player().gameprofile.name))
-                .color_named(NamedColor::Green),
-        );
-    }
-}
+use pumpkin::plugin::Context;
+use pumpkin_api_macros::{plugin_impl, plugin_method};
 
 #[plugin_method]
 async fn on_load(&mut self, server: &Context) -> Result<(), String> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     log::info!("Hello, Pumpkin!");
-
-    server.register_event(MyJoinHandler, EventPriority::Lowest, true).await;
 
     Ok(())
 }
